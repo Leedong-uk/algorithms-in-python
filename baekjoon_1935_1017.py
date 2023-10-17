2e3f6c180c9edc8b6e635acf60277a005750ec2a
이동욱 <du123kim@naver.com>
2023-10-17 13:43:54 +0900

    1017_commit

---
+++ baekjoon_1935_1017.py
@@ -0,0 +1,58 @@
+n = int(input()) 
+s = list(input())
+stack = []
+a= 0
+
+num = [None] * n
+
+for i in range(n) : 
+    tmp = input()
+    num[i] = tmp
+
+
+for i in range(len(s)) : 
+    if s[i].isalnum() : 
+        stack.append(num[a])
+       
+        if n == 1 :
+            pass
+        else : 
+            a= a+1
+    else : 
+        if s[i] == "/" : 
+            n1 = stack.pop()
+            n2 = stack.pop()
+            n3 = str(eval(n2+s[i]+n1))
+            stack.append(n3)
+            
+        
+        elif s[i] == "-" : 
+            n1 = stack.pop()
+            n2 = stack.pop()
+            n3 = str(abs(eval(n2+s[i]+n1)))
+            stack.append(n3)
+            
+        else : 
+            n1 = stack.pop()
+            n2 = stack.pop()
+            n3 = str(eval(n1+s[i]+n2))
+            stack.append(n3)
+            
+
+
+print("%0.2f"%float(stack[0]))
+
+
+
+n = int(input())
+word = input()
+num_list = [0]*n
+
+for i in range(n) : 
+        num_list[i] = int(input())
+
+stack = []
+
+for i in word : 
+        if i.isalnum() : 
+                stack.append(num[])
