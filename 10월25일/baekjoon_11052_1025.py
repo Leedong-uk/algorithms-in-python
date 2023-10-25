bd75949611bf4ef82f405c3415e71319fe87ab3d
이동욱 <du123kim@naver.com>
2023-10-25 15:14:20 +0900

    1025_commit

---
+++ 10월25일/baekjoon_11052_1025.py
@@ -0,0 +1,11 @@
+N = int(input())
+lst = [0]+list(map(int, input().split()))
+
+
+dp = [0]*(N+1)
+
+for i in range(1, N+1):     
+    for j in range(1, N+1): 
+        if j-i>=0:         
+            dp[j]=max(dp[j], dp[j-i]+lst[i])
+print(dp[N])
