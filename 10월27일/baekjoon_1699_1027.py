f88e92cd12e90c2aa5c6fae35c5b2e1f08abac28
이동욱 <du123kim@naver.com>
2023-10-27 15:03:10 +0900

    1027_cOmmit😍

---
+++ 10월27일/baekjoon_1699_1027.py
@@ -0,0 +1,10 @@
+from math import sqrt
+N = int(input())
+dp = [i for i in range(N +1)]
+
+for i in range(1,N+1) : 
+    for j in range(1,int(sqrt(i)+1)) : 
+        if dp[i]>(dp[i-j*j]+1): 
+            dp[i] = dp[i-j*j] +1
+
+print(dp[N])
