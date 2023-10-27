1d2088fd491d5edd8d1cb7000d33798fee80423a
이동욱 <du123kim@naver.com>
2023-10-27 12:02:21 +0900

    1027_cOmmit 😍

--- 10월27일/baekjoon_2193_1027.py
+++ 10월27일/baekjoon_2193_1027.py
@@ -2,13 +2,4 @@ N = int(input())
 
 dp = [[0]*2 for _ in range(N+1)]
 
-dp[1] = [0,1]
-
-for i in range(2,N+1) : 
-    dp[i][0] = dp[i-1][0]+dp[i-1][1]
-    dp[i][1] = dp[i-1][0]
-
-
-ans = sum(dp[N])
-
-print(ans)
+dp[1] = [0,1] 
