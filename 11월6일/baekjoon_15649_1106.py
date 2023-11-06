5acebe5bcb07698f458ad96bdeb91ddaeeef2921
이동욱 <du123kim@naver.com>
2023-11-06 16:07:30 +0900

    1106_commit

---
+++ 11월6일/baekjoon_15649_1106.py
@@ -0,0 +1,19 @@
+n , m = map(int,input().split())
+
+check = [False]*(n+1)
+ans = [0]*(m)
+
+def go(index,n,m) : #index = 현재 숫자를 넣을 자리수  
+    if index == m : #n : 숫자의 범위 
+        print(" ".join(map(str,ans)))
+        return      #m : 순열의 자리수
+    
+    for i in range(1,n+1) : 
+        if check[i] : 
+            continue 
+        check[i] = True 
+        ans[index] = i
+        go(index+1,n,m)
+        check[i] = False
+
+go(0,n,m)
