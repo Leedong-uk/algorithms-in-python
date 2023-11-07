53fd1ac862e5122c042381027b6bb2a541d99f52
이동욱 <du123kim@naver.com>
2023-11-07 15:46:27 +0900

    1107_commit

---
+++ 11월7일/baekjoon_15656_1107.py
@@ -0,0 +1,21 @@
+n , m = map(int,input().split())
+num = list(map(int,input().split()))
+num.sort()
+
+check = [False] * n
+ans = [0] *m
+
+def go (index,n,m) : 
+    if index == m : 
+        print(" ".join(map(str,ans)))
+        return
+    
+    for i in range(n) : 
+        
+        check[i] = True
+        ans[index] = num[i]
+        go(index+1,n,m)
+        check[i] = False
+
+
+go(0,n,m)
