6b21c8f7f56f5e35128d46ae6b816925dfd4d892
이동욱 <du123kim@naver.com>
2023-11-10 19:06:30 +0900

    1110_commit

--- 11월10일/baekjoon_2529_1110.py
+++ 11월10일/baekjoon_2529_1110.py
@@ -1,4 +1,7 @@
 #내일 다시 시도 해 보자!
+#범위가 달라서 이전에 했던 방식으로는 힘듬
+#일단 손코드를 해보면서 왜 2로 시작하는 애들은 tmp에 저장이 안되었는지
+#그리고 그걸 확인했으면 n과 m 방식으로 다시 풀어보자 
 k = int(input())
 moon = list(map(str,input().split()))
 s = ""
@@ -15,12 +18,16 @@ def go (index,n,s) :
         
         return
     
-    for _ in range(1,10):
-        go(index+1,n+1,s+str(index))
-        go(index+1,n,s)
+    
+    go(index+1,n+1,s+str(index))
+    print("index : %d"%index)
+    print("s = ",end= " ")
+    print(s)
+    go(index+1,n,s)
 
 
-go(1,0,s)
+go(1,0,"")
+print(tmp)
 print(max(tmp))
 print(min(tmp))
         
