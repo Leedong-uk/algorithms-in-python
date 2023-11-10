c7a8764bd32dad5781588220d18cc55d94812ad1
이동욱 <du123kim@naver.com>
2023-11-10 18:50:33 +0900

    1110_commit

---
+++ 11월10일/baekjoon_2529_1110.py
@@ -0,0 +1,25 @@
+k = int(input())
+moon = list(map(str,input().split()))
+s = []
+tmp = []
+
+def go (index,n,s) : 
+    if index == k+2 : 
+        if n == k+1 : 
+            for i in range(k) : 
+                x = eval(s[i]+moon[i]+s[i+1])
+                if x : 
+                    tmp.append(s)
+                    return
+        
+        return
+    
+
+    go(index+1,n+1,s+[str(index)])
+    go(index+1,n,s)
+
+
+go(1,0,s)
+print(max(tmp))
+print(min(tmp))
+        
