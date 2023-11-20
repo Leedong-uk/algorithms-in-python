ac720fa24e50da2292e291a2d785f8ef61bf5a64
이동욱 <du123kim@naver.com>
2023-11-20 17:40:55 +0900

    1120_commit

--- 11월20일/boj_11723.py
+++ 11월20일/boj_11723.py
@@ -1,35 +1,24 @@
-#비트마스크 연습
 import sys
-n=20
+n = 20
 m = int(sys.stdin.readline())
 s = 0
-
-for _ in range(m) : 
-    cmd =  sys.stdin.readline().split()
-
-    if cmd[1]  : 
-        x = int(cmd[1])-1
-
-    if cmd[0] == "add" : 
-        s = (s | (1<<x))
-    
-    elif cmd[0] == "remove" : 
-        s = (s & ~(1<<x))
-
-    elif cmd[0] == "check" : 
-        check = (s &(1 << x))
-        if check >0 : 
+for _ in range(m):
+    op, *num = sys.stdin.readline().split()
+    if len(num) > 0:
+        x = int(num[0])-1
+    if op == 'add':
+        s = (s | (1 << x))
+    elif op == 'remove':
+        s = (s & ~(1 << x))
+    elif op == 'check':
+        res = (s & (1 << x))
+        if res > 0:
             sys.stdout.write('1\n')
-        else : 
+        else:
             sys.stdout.write('0\n')
-    
-    elif cmd[0] == "toggle" : 
-        s = (s ^ (1<<x))
-    
-    elif cmd[0] =="all" : 
-        s = (1<<n)-1
-    
-    else : 
+    elif op == 'toggle':
+        s = (s ^ (1 << x))
+    elif op == 'all':
+        s = (1 << n) - 1
+    else:
         s = 0
-    
-   
