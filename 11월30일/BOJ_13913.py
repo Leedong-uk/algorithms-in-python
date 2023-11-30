b3deb8290535ecdc0d382ce4cb38b69639396f7d
이동욱 <du123kim@naver.com>
2023-11-30 15:30:33 +0900

    1130_commit

---
+++ 11월30일/BOJ_13913.py
@@ -0,0 +1,43 @@
+from collections import deque
+MAX = 200000
+start , end = map(int,input().split())
+check = [False] *(MAX+1)
+dist = [-1]*(MAX+1)
+q = deque()
+
+a = [[] for _ in range(MAX+1)]
+tmp = []
+
+check[start] = True
+dist[start] = 0
+q.append(start)
+
+
+while q : 
+    now = q.popleft()
+    for next in [now-1,now+1,now*2] : 
+        if 0<=next <MAX and check[next] == False :
+            check[next]= True
+            dist[next] = dist[now]+1
+            a[now].append(next)
+            a[next].append(now)
+            q.append(next)
+
+
+
+
+
+tmp.append(end)
+def ans(end) : 
+    cnt = dist[end]    
+    for i in a[end] : 
+        if dist[i] < cnt : 
+            tmp.append(i)
+            ans(i)
+
+ans(end)
+tmp.reverse()
+print(dist[end])
+print(" ".join(map(str,tmp)))
+
+
