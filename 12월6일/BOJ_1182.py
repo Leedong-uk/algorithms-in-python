f7077cd7fae6564009015b7015019803bc13f75b
이동욱 <du123kim@naver.com>
2023-12-06 15:30:44 +0900

    1206_commit

--- 12월6일/BOJ_1182.py
+++ 12월6일/BOJ_1182.py
@@ -1,6 +1,8 @@
+#부분집합을 구할떈 이렇게 구하자 길이가 계속 달라지니깐 이게 가장 편함
 n,m = map(int,input().split())
 a = list(map(int,input().split()))
 ans = 0
+
 def go(i, s):
     global ans
     if i == n:
@@ -9,7 +11,12 @@ def go(i, s):
         return
     go(i+1,s+a[i])
     go(i+1,s)
+
+
 go(0, 0)
+
+
 if m == 0:
     ans -= 1
+
 print(ans)
