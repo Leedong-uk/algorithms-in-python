9ef26e74242f5221db6cbf880061b209df08de34
이동욱 <du123kim@naver.com>
2023-12-12 17:24:18 +0900

    1212

---
+++ 12월12일/BOJ_14052.py
@@ -0,0 +1,73 @@
+# 0 : 빈칸 , 1 : 벽 , 2 : 바이러스 
+# 바이러스가 퍼질수 있는 영역의 수 : bfs가 방문한 칸의수
+# 바이러스가 퍼질수 없는 영역의수 : 전체 - bfs가 방문한 칸의수
+# 시간복잡도 : 최대 NM개의 칸을 방문할수있으니 O(NM)이다
+# 벽을 세우는 경우의수 (NM)^3 하고 BFS 나 DFS 하는 시간복작도 NM
+# 총 O((NM)^5) = 2^25 = 33,554,432
+# 보통 1억이 1초
+from collections import deque
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+
+def bfs(a):
+    n = len(a)
+    m = len(a[0])
+    b = [[0]*m for _ in range(n)]
+    q = deque()
+    for i in range(n):
+        for j in range(m):
+            b[i][j] = a[i][j]
+            if b[i][j] == 2:
+                q.append((i,j))
+    while q:
+        x,y = q.popleft()
+        for k in range(4):
+            nx,ny = x+dx[k], y+dy[k]
+            if 0 <= nx < n and 0 <= ny < m and b[nx][ny] == 0:
+                b[nx][ny] = 2
+                q.append((nx,ny))
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if b[i][j] == 0:
+                cnt += 1
+    return cnt
+
+
+n,m = map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(n)]
+ans = 0
+
+for x1 in range(n):
+    for y1 in range(m):
+        if a[x1][y1] != 0:
+            continue
+        for x2 in range(n):
+            for y2 in range(m):
+                if a[x2][y2] != 0:
+                    continue
+                for x3 in range(n):
+                    for y3 in range(m):
+                        if a[x3][y3] != 0:
+                            continue
+                        if x1 == x2 and y1 == y2:
+                            continue
+                        if x1 == x3 and y1 == y3:
+                            continue
+                        if x2 == x3 and y2 == y3:
+                            continue
+                        a[x1][y1] = 1
+                        a[x2][y2] = 1
+                        a[x3][y3] = 1
+                        cur = bfs(a)
+                        
+                        if ans < cur:
+                            ans = cur
+                        a[x1][y1] = 0
+                        a[x2][y2] = 0
+                        a[x3][y3] = 0
+print(ans)
+
+
+
