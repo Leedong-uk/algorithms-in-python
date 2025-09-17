27ed73fb0e873d36246348806e2d88478c06ba19
이동욱 <du123kim@naver.com>
2025-09-17 12:49:49 +0900

    2025-09-17 코테

---
+++ 2025/9월/9월17일/LC_ValidParentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for i in s:
+            if i in bracket_map:  
+                if stack and stack[-1] == bracket_map[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:  
+                stack.append(i)
+
+        return len(stack) == 0 
+        
