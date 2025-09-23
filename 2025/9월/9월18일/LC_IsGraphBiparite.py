23a29f7d4fc465b9436d9c4ca7bba5e7c55f8346
동욱 <du123kim@naver.com>
2025-09-23 14:37:06 +0900

    2025-09-23

--- 2025/9월/9월18일/LC_IsGraphBiparite.py
+++ 2025/9월/9월18일/LC_IsGraphBiparite.py
@@ -2,24 +2,24 @@ from collections import deque
 def isBipartite(graph) : 
     q = deque()
     n = len(graph)
-    color = [False] * n
-    color[0] = 0
-    q.append(0)
-    
-    while q : 
-        x = q.popleft()
-        curr_color = color[x]
-        for i in graph[x] : 
-            if not color[i]  : 
-                color[i] = (curr_color +1)%2
-                q.append(i)
-            else : 
-                if curr_color == color[i] : 
-                    return False
-    print(color)
-    
+    color = [-1] * n
+    for start in range(n) : 
+        if color[start] == -1 : 
+            q.append(start)
+            color[start] = 0
+        while q : 
+            x = q.popleft()
+            curr_color = color[x]
+            for i in graph[x] : 
+                if color[i] == -1  : 
+                    color[i] = (curr_color +1)%2
+                    q.append(i)
+                else : 
+                    if curr_color == color[i] : 
+                        return False
+        
     return True
-
+    
 
 print(isBipartite([[1,2,3],[0,2],[0,1,3],[0,2]]))
 print (isBipartite([[1,3],[0,2],[1,3],[0,2]]))
